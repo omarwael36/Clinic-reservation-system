9377bb2f9e1c91cfb69287f60bb9ee7201d6403d
omarwael36 <119500973+omarwael36@users.noreply.github.com>
2023-12-20 21:55:41 +0200

    trial #1000

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from './api.service';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,7 +1,7 @@
 // src/environments/environment.ts
+import Config from "../config.json";
 
 export const environment = {
     production: false,
-    apiUrl: 'https://clinic-reservation-system-git-omarwael20021-dev.apps.sandbox-m3.1530.p1.openshiftapps.com',
-  };
-  
+    apiUrl: Config.API_URL,
+};
