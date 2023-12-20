ea48b9943d86a521493ff238395a1017097bebe9
omarwael36 <119500973+omarwael36@users.noreply.github.com>
2023-12-20 21:34:16 +0200

    final revert

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,7 +1,7 @@
 // src/environments/environment.ts
 
 export const environment = {
-  production: false,
-  apiUrl: 'lol'
-};
-
+    production: false,
+    apiUrl: 'https://clinic-reservation-system-git-omarwael20021-dev.apps.sandbox-m3.1530.p1.openshiftapps.com',
+  };
+  
