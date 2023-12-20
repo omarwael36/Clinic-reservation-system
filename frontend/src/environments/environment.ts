19c7674046c91a66c534b198dd1a979e5cb67455
omarwael36 <119500973+omarwael36@users.noreply.github.com>
2023-12-20 20:40:25 +0200

    trial

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,7 @@
 // src/environments/environment.ts
 
 export const environment = {
-    production: false,
-    apiUrl: 'https://clinic-reservation-system-git-omarwael20021-dev.apps.sandbox-m3.1530.p1.openshiftapps.com', 
+  production: false,
+  apiUrl: process.env["URL"] || 'lol',
 };
+
